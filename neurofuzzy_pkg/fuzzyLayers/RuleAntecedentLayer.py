does not run with normal dummy anymore, mistake in scaling to more features

neurofuzzy_pkg/fuzzyLayers/RuleAntecedentLayer.py
@@ -37,7 +37,7 @@ class RuleAntecedentLayer():
         # for rule extraction
         self.rulesIF = {}   
         self.n_rules = 0     
-        self.n_participants = 4 # two participants in a rule #hc
+        self.n_participants = 2 # two participants in a rule #hc
         self.n_mfs = 3 # hc
 
 
@@ -88,11 +88,13 @@ class RuleAntecedentLayer():
 
        
         x = np.array_split(x, range(3, len(x), 3))
-        x.reverse()  # so it fits with convention 
-        x = np.array(np.meshgrid(*x)) # the '*' unpacks x and passes to messgrid
+       # x.reverse()  # so it fits with convention 
+        x = np.array(np.meshgrid(*x,indexing='ij')) # the '*' unpacks x and passes to messgrid
         self.inputs = x #  need meshgrid for training
 
-        x = np.prod(x, axis=0).ravel()
+
+        x = (x[0] * x[1]).ravel()
+       # x = np.prod(x, axis=0).ravel()
   
 
         assert self.n_rules == x.size, f'the number of rules generated: {x.size} has to equal: {self.n_rules} -> coefficient(inputs * n_mfs, participants) - inputs * n_mfs' 

neurofuzzy_pkg/utils/MyArcTrainer.py
@@ -265,33 +265,37 @@ class MyArcTrainer(Trainer):
 
         deltas = []
 
-            
-        x = np.sum(delta[0], axis=3)
-        x = np.sum(x, axis=1)
-        x = np.sum(x, axis=1)
-        deltas.append(x)
+        deltas = [np.sum(d, axis=(i+1)%2) for i, d in enumerate(delta)]
+        deltas = np.array(deltas)
+        deltas = deltas.ravel()
+        # x = delta[0]
+        # x = np.sum(x, axis=3)
+        # x = np.sum(x, axis=1)
+        # x = np.sum(x, axis=1)
+        # deltas.append(x)
 
-        y = np.sum(delta[1], axis=2)
-        y = np.sum(y, axis=1)
-        y = np.sum(y, axis=1)
-        deltas.append(y)
+        # x = delta[1]
+        # x = np.sum(x, axis=0)
+        # x = np.sum(x, axis=1)
+        # x = np.sum(x, axis=1)
+        # deltas.append(x)
         
-        z = np.sum(delta[2], axis=0)
-        #print("x", x)
-        z = np.sum(z, axis=1)
-        z = np.sum(z, axis=1)
-        deltas.append(z)
+        # x = delta[2]
+        # x = np.sum(x, axis=0) # or 1
+        # x = np.sum(x, axis=2)
+        # x = np.sum(x, axis=0)
+        # deltas.append(x)
 
-        w = np.sum(delta[3], axis=1)
-        #print("x", x)
-        w = np.sum(w, axis=1)
-        w = np.sum(w, axis=1)
-        deltas.append(w)
+        # x = delta[3]
+        # x = np.sum(x, axis=1) # or 2
+        # x = np.sum(x, axis=0)
+        # x = np.sum(x, axis=0)
+        # deltas.append(x)
 
         # deltas = [np.sum(d, axis=3-(i%2)) for i, d in enumerate(delta)]
         # deltas = [np.sum(d, axis=(1,2)) for i, d in enumerate(deltas)]
-        deltas = np.array(deltas)
-        deltas = deltas.ravel()
+       # deltas = np.array(deltas)
+       # deltas = deltas.ravel()
         print("deltas", deltas)
         print("para", para_prime)
         deltas *= para_prime