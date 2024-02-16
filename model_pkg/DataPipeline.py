scaling issues fixed
in builder, antecedent, consequent, mfs, ruleminer ...

model_pkg/DataPipeline.py
@@ -27,6 +27,7 @@ class DataPipeline():
             split of dataset is 80:10:10
         """
         self.df_name = df_name
+        self.df_names = df_names = ['dummy2', 'dummy3', 'dummy4', 'iris']
         self.batch_size = batch_size
         self.params = [self.batch_size]
         
@@ -66,6 +67,11 @@ class DataPipeline():
         Note: assumes last column of df is the target vector
         """
         # check if str is in possible datasets
+        
+        if self.df_name not in self.df_names:
+            raise ValueError(
+                f"Valid values for df name are {self.df_names}.")
+    
         file_name = self.df_name + '_df.csv'
         save_path = os.path.dirname(__file__) +  '/../data'
         full_path = os.path.join(save_path, file_name)
@@ -79,6 +85,8 @@ class DataPipeline():
         self.feature_names = list(df.columns)
         # get max value of each feature <- center init        
         self.feature_ranges = df.max()
+        self.n_features = len(self.feature_names)
+        self.n_classes = len(np.unique(targets))
         print(f"Dataset {self.df_name} loaded: \n {df.head()} \n")
 
         return df,targets
@@ -126,6 +134,8 @@ class DataPipeline():
         self.feature_names = list(df.columns)
         # get max value of each feature <- center init
         self.feature_ranges = df.max()
+        self.n_features = len(self.feature_names)
+        self.n_classes = len(np.unique(targets))
         return df, targets
 
 
@@ -142,8 +152,8 @@ class DataPipeline():
        # targets = targets.apply(lambda x: int(x >= treshhold))
 
         # one hot encoding
-        depth = 2 # DANGER HC
-        b = tf.one_hot(targets, depth)
+        #depth = 2 # DANGER HC
+        b = tf.one_hot(targets, self.n_classes)
         targets = b.numpy()
 
         self.inputs = df.to_numpy()# df.map(lambda x,y: x)
@@ -164,8 +174,7 @@ class DataPipeline():
         self.generate_folders(self.df_name) 
 
         # one hot encoding
-        depth = 2
-        b = tf.one_hot(targets, depth)
+        b = tf.one_hot(targets, self.n_classes)
         targets = b.numpy()
 
         df = df.to_numpy()

neurofuzzy_pkg/fuzzyLayers/RuleAntecedentLayer.py
@@ -37,7 +37,7 @@ class RuleAntecedentLayer():
         # for rule extraction
         self.rulesIF = {}   
         self.n_rules = 0     
-        self.n_participants = 4 # two participants in a rule #hc
+        self.n_features = 4 # two participants in a rule #hc
         self.n_mfs = 3 # hc
 
 
@@ -81,7 +81,7 @@ class RuleAntecedentLayer():
 
     
         
-        self.n_rules = int(self.n_mfs**self.n_participants)
+        self.n_rules = int(self.n_mfs**self.n_features)
 
         # x = np.array_split(x, range(self.n_mfs, len(x), self.n_mfs))
 

neurofuzzy_pkg/utils/Builder.py
@@ -34,6 +34,7 @@ class Builder():
         """
         self.build_MFs(feature_ranges, df_name)
         self.build_classweights(inputs, targets, df_name)
+    
         return True       
     
 
@@ -53,7 +54,7 @@ class Builder():
         #  print(n_inputs)
 
 
-        n_rules = int(self.arc.RuleAntecedentLayer.n_mfs**self.arc.RuleAntecedentLayer.n_participants )
+        n_rules = int(self.arc.RuleAntecedentLayer.n_mfs**self.arc.RuleAntecedentLayer.n_features )
 
         for i in range(n_rules):
             self.arc.RuleConsequentLayer.dictrules[i] = []
@@ -70,8 +71,10 @@ class Builder():
         for ruleID in tqdm(self.arc.RuleConsequentLayer.dictrules, desc="selecting"):
             l = self.arc.RuleConsequentLayer.dictrules[ruleID]
           
-            max_val = max(l)
-            idx_max = l.index(max_val)
+          #  max_val = max(l)
+           # idx_max = l.index(max_val)
+
+            idx_max = np.argmax(l)
             
             tar = self.arc.RuleConsequentLayer.tars[ruleID][idx_max]
            # print("tar", tar)

neurofuzzy_pkg/utils/MFs.py
@@ -138,12 +138,12 @@ Args:
     
     feature_names = max_vals.keys().values.tolist()
     
-    c = np.array_split(layer.centers, 2) # hc
+    c = np.array_split(layer.centers, len(feature_names)) # hc
    # print("HERE", c)
  
-    w = np.array_split(layer.widths,  2) # hc
+    w = np.array_split(layer.widths, len(feature_names)) # hc
  
-
+    print("max", max_vals)
    
    # print("c", c)
     #print("w", w)
@@ -152,18 +152,20 @@ Args:
     for xID, max_value in enumerate(max_vals):
     #for name, input in zip(names,layer.inputs):
 #        x = np.arange(0, means[xID] , (means[xID]*0.01))
-        x = np.arange(0, max_value, 0.01)
+        x = np.arange(0, max_value, max_value/1000)
         y = {}
 
         for mfID in range(layer.n_mfs):
             y[mfID] = []
 
 #            print(layer.centers[j+i*layer.n_mfs])
-            for bleh in x:
+         #   for bleh in x:
               #  print(c[xID])
               #  print(c[xID][mfID])
-                y[mfID].append(layer.mf_type(bleh,c[xID][mfID],w[xID][mfID]))
+            y[mfID] =layer.mf_type(x,c[xID][mfID],w[xID][mfID])
 
+           # print("x", x)
+            #print("y,", y[mfID])
             plt.plot(x, y[mfID], label=mf_names[mfID])
 
           #  print("hui", c[xID][mfID])

neurofuzzy_pkg/utils/MyArcTrainer.py
@@ -33,6 +33,7 @@ class MyArcTrainer(Trainer):
         self.learning_rate = learning_rate
         self.feature_ranges = None
         self.builder = None
+        self.max_vals = None
       #  self.df_name = None
        # self.feature_ranges = None 
 
@@ -370,25 +371,25 @@ class MyArcTrainer(Trainer):
             x = np.sum(x, axis=3)
             x = np.sum(x, axis=1)
             x = np.sum(x, axis=1)
-            deltas.append(x)
+            deltas.append(x/81 * self.learning_rate)
 
             x = delta[1]
             x = np.sum(x, axis=0)
             x = np.sum(x, axis=1)
             x = np.sum(x, axis=1)
-            deltas.append(x)
+            deltas.append(x/81 * self.learning_rate)
             
             x = delta[2]
             x = np.sum(x, axis=0) # or 1
             x = np.sum(x, axis=2)
             x = np.sum(x, axis=0)
-            deltas.append(x)
+            deltas.append(x/81 * self.learning_rate)
 
             x = delta[3]
             x = np.sum(x, axis=1) # or 2
             x = np.sum(x, axis=0)
             x = np.sum(x, axis=0)
-            deltas.append(x)
+            deltas.append(x/81 * self.learning_rate)
 
         deltas = np.array(deltas)
         deltas = deltas.ravel()
@@ -433,13 +434,17 @@ class MyArcTrainer(Trainer):
        # print("param afte", para)
 
 
-      #  print("heh", param_to_tune)
-        
+      #  print("heh", para)
+        n_mfs = 3 #hc
+       # print("HOOONK",self.max_vals)
+        hmm = np.repeat(self.max_vals, n_mfs)
+        #print("honik",hmm)
         for i, p in enumerate(para):
-          #  print("p", p)
-            if p <= 0 or p >= 10: #hc
+          #  for j in n_mfs:
+        #  print("p", p)
+            if p <= 0 or p >= hmm[i]: #hc
                 deltas[i] = 0#0.00001 # randomize todo
-               # print("P",p)
+            # print("P",p)
        # print("deltas", deltas)
         para =  para - deltas #np.multiply(deltas, self.learning_rate)
         setattr(layer, param, para)

neurofuzzy_pkg/utils/RuleMiner.py
@@ -18,7 +18,7 @@ class RuleMiner():
     ___________________________________________________________
     """
 
-    def __init__(self, neuro_fuzzy_model, mlp_model, df_name):
+    def __init__(self, neuro_fuzzy_model, df_name):
         """Init ruleExtractor and calling extractRules()
 
         Args:
@@ -28,15 +28,16 @@ class RuleMiner():
 
         # arcs used
         self.arc = neuro_fuzzy_model.arc
-        self.classifier = mlp_model 
+        self.classifier = None#mlp_model 
 
         # for final Dict of rules
         self.rulesDict = {}
         self.feature_names = neuro_fuzzy_model.data.feature_names
         self.linguistic_mf = ["low","medium","high"]
-        self.lingusitic_output = ["bad", "good"]
-        self.n_participants = 4
-        self.n_outputs = 2# hc neuro_fuzzy_model.arc.RuleConsequentLayer.n_mfs 
+        self.lingusitic_output = ["Setosa", "Versicolour", "Virginica"]# ["bad", "good"]
+        self.n_mfs = len(self.linguistic_mf)
+        self.n_participants = len(neuro_fuzzy_model.data.feature_names)
+        self.n_outputs = len(self.lingusitic_output)# hc neuro_fuzzy_model.arc.RuleConsequentLayer.n_mfs 
         self.df_name = df_name
 
         # calling functions
@@ -47,34 +48,40 @@ class RuleMiner():
 
         
         feature_names = np.asarray(feature_names)
-        feature_names = np.tile(feature_names[:, np.newaxis], 3).ravel()
+        feature_names = np.tile(feature_names[:, np.newaxis], self.n_mfs).ravel()
 
         mfs = np.tile(mfs, self.n_participants)
 
       #  fuzzy = np.char.add(feature_names, ",") 
        # fuzzy = np.char.add(fuzzy, mfs) 
-        fuzzy = np.char.add(mfs, ",") 
+      #  fuzzy = np.char.add(mfs, ",") 
 
   
-        fuzzy = np.array_split(fuzzy, range(3, len(fuzzy), 3))
+        mfs = np.array_split(mfs, range(3, len(mfs), self.n_mfs))
         #x.reverse()
-        fuzzy = np.stack(np.meshgrid(*fuzzy, indexing='ij'), axis=-1).reshape(-1, self.n_participants)
+        mfs = np.stack(np.meshgrid(*mfs, indexing='ij'), axis=-1).reshape(-1, self.n_participants)
 
 
-        return fuzzy#.tolist()
+        return mfs#.tolist()
     
     def get_then_part(self):
         weights = self.arc.RuleConsequentLayer.weights
-        print("weights", weights)
-        neww = np.where(weights[:, 0] == 1.0, 0, 1)
-       
-
+      #  print("weights", weights)
+        
+        output = []
+        for w in weights:
+            print("w", w)
+            idx_max = np.argmax(w)
+            output.append(self.lingusitic_output[idx_max])
+               
+        output = np.array(output)
+        output = output.reshape(-1, 1)
         # Reshape the array to have a single column
-        neww = neww.reshape(-1, )
-        neww = neww.T
-        neww = neww.reshape(-1, 1)
+       # neww = neww.reshape(-1, )
+      #  neww = neww.T
+      #  neww = neww.reshape(-1, 1)
        # neww = neww.tolist()
-        return neww
+        return output
 
 
     def extractRules(self):
@@ -99,12 +106,12 @@ class RuleMiner():
         # good yield -> outmfID = 1 
         # bad yield -> outmfID = 0
         rulesIF = self.get_if_part(self.feature_names, self.linguistic_mf)
-        print("if", rulesIF)
+    #    print("if", rulesIF)
         rulesTHEN = self.get_then_part()
-        print("then", rulesTHEN)
+     #   print("then", rulesTHEN)
        # rulesIF = np.char.add(rulesIF, ",") 
         huh = np.concatenate((rulesIF, rulesTHEN), axis=1)# #np.char.add(rulesIF, rulesTHEN) 
-        print("huh", huh)
+      #  print("huh", huh)
         #huh = huh[0]
 
             
@@ -137,15 +144,15 @@ class RuleMiner():
      
 
         # Create a pandas DataFrame
-        self.feature_names.append("Target")
-        print("honk", self.feature_names)
+        self.feature_names.append("Class")
+   #     print("honk", self.feature_names)
         df =      pd.DataFrame(huh, columns=self.feature_names)
 
         # Display the resulting DataFrame
-        print(df)
+    #    print(df)
         self.rulesDict = df
 
-        print("/n dict", self.rulesDict)
+     #   print("/n dict", self.rulesDict)
         self.save_results()
       #  self.print_results()
 